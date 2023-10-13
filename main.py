d26ab28ea3c312ba5e0ce82ae6f537fc2f2aa25b
olivia svensson <oliviateoliva@gmail.com>
2023-10-14 00:13:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ chars = list(chars)
 key = chars.copy()
 
 random.shuffle(key)
-print(f"chars: {chars}")
-print(f"key: {key}")
+#print(f"chars: {chars}")
+#print(f"key: {key}")
 
 #ENCRYPTION
 plain_text = input("Enter a message to encrypt: ")
@@ -19,3 +19,16 @@ for letter in plain_text:
 
 print(f"original message: {plain_text}")
 print(f"encrypted message: {cipher_text}")
+
+#DECRYPTION
+plain_text = input("Enter a message to decrypt: ")
+cipher_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"encrypted message: {cipher_text}")
+print(f"original message: {plain_text}")
+
+
