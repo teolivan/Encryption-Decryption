90462cb041553c52b0e213bf5c7aba044beeb82e
olivia svensson <oliviateoliva@gmail.com>
2023-10-14 00:10:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ print(f"key: {key}")
 
 #ENCRYPTION
 plain_text = input("Enter a message to encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"original message: {plain_text}")
+print(f"encrypted message: {cipher_text}")
